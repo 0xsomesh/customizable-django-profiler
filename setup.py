cd87b62eef01d433132393b3acfe6fa7e024b10c
hulksmasher <somesh.08.96@gmail.com>
2017-06-27 00:17:47 +0530

    dj to django

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-        name='dj_profiler',
+        name='django_profiler',
         version='0.1',
         url='https://github.com/someshchaturvedi/dj_profiler.git',
         description='Django middleware based on cProfile',
         author='Somesh Chaturvedi',
         author_email='somesh.08.96@gmail.com',
         license='MIT',
-        packages=['dj_profiler'],
+        packages=['django_profiler'],
         keywords = ['django','middleware','cProfile', 'Profile'],
         zip_safe=False
       )
