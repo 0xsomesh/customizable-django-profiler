9606fb93853c61a689b69bd0783f557148a06736
hulksmasher <somesh.08.96@gmail.com>
2017-06-27 01:21:15 +0530

    download_url added

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(
         name='django_profiler',
         version='0.1',
-        url='https://github.com/someshchaturvedi/dj_profiler.git',
+        url='https://github.com/someshchaturvedi/django_profiler',
         description='Django middleware based on cProfile',
         author='Somesh Chaturvedi',
         author_email='somesh.08.96@gmail.com',
         license='MIT',
         packages=['django_profiler'],
         keywords = ['django','middleware','cProfile', 'Profile'],
-        zip_safe=False
+        zip_safe=False,
+        download_url = 'https://github.com/someshchaturvedi/django_profiler/archive/0.1.tar.gz'
       )
